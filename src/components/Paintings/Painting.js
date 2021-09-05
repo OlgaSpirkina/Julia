8c765fbebd715fbf7406d61123c674085067357f
olga <>
2021-09-05 22:37:24 +0200

    add background imgs to Gallery

--- src/components/Paintings/Painting.js
+++ src/components/Paintings/Painting.js
@@ -2,14 +2,12 @@ import PropTypes from 'prop-types'
 import styles from './Paintings.module.css'
 
 const Painting = ({ paint: { image, title} }) => {
-  const verticalImg = title.includes('vpaint');
   return(
     <figure className={styles.paint_container}>
       <figcaption className={styles.figcaption}>{title}</figcaption>
       <img
         src={image}
         alt={title}
-        className={(verticalImg === true) ? `${styles.vertical}` : `${styles.horizontal}` }
       />
     </figure>
   )
