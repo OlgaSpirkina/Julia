c5f847283f54b291ed911767578cc1dc9536de3b
olga <>
2021-09-06 18:11:12 +0200

    add some style to Gallery; add button go back to Details

--- src/components/ArrowUp/index.js
+++ src/components/ArrowUp/index.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import { MdArrowUpward } from 'react-icons/md'
 import styles from './ArrowUp.module.css'
 
--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import { useParams } from 'react-router'
 import Detail from './Detail'
 import styles from './Details.module.css'
@@ -18,6 +19,28 @@ const Details = ({ paintings }) =>{
 
   return(
     <section className={`${styles.section} ${styles.details_container}`}>
+      <div
+        style={{
+          width: '75%',
+          display: 'flex',
+          justifyContent: 'flex-end'
+      }}
+      >
+        <Link to="/gallery">
+          <button
+            style={{
+              border: 'none',
+              backgroundColor: 'var(--primaryOrange)',
+              color: '#fff',
+              padding: '.5rem',
+              borderRadius: '.5rem',
+              cursor: 'pointer',
+              fontSize: '1rem'
+             }}>
+            Go to Gallery
+          </button>
+        </Link>
+      </div>
       {filteredByCategory.map((elem, i) => {
         return(
           <div key={i} className={styles.detail_text_img}>
