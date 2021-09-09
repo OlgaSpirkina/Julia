191be9b8bebf1b22b8de172c3fa35b4e44a81d00
olga <>
2021-09-09 13:36:18 +0200

    add state to label text; place navbar correctly in Contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,6 +5,11 @@ const Contact = () => {
   // Regex
   const [email, setEmail] = useState('');
   const [emailErr, setEmailErr] = useState(false);
+  const [labelForName, setLabelForName] = useState('');
+  const [labelForEmail, setLabelForEmail] = useState('');
+  const [labelForCompany, setLabelForCompany] = useState('');
+  const [labelForPhone, setLabelForPhone] = useState('');
+  const [labelForMessage, setLabelForMessage] = useState('');
   const validEmail = new RegExp(
    '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$'
  );
@@ -19,31 +24,82 @@ const Contact = () => {
       <div>
         <form className={styles.contact_form} action="https://formspree.io/f/xwkaewbb" method="POST">
           <div className={styles.form_field}>
-            <input className={styles.input_text} id="name" type="text" name="name" required="required" />
-            <label className={styles.label} htmlFor="name">name</label>
+            <input
+              onChange={(e) => setLabelForName(e.target.value)}
+              className={styles.input_text}
+              id="name"
+              type="text"
+              name="name"
+              required="required" />
+            <label
+              className={styles.label}
+              htmlFor="name"
+            >
+              {labelForName ? '' : 'name'}
+            </label>
           </div>
           <div className={styles.form_field}>
             <input
               onChange={(e) => setEmail(e.target.value)}
+              onChange={(e) => setLabelForEmail(e.target.value)}
               className={styles.input_text}
               id="email"
               type="email"
               name="email"
               required="required"
             />
-            <label className={styles.label} htmlFor="email">email</label>
+            <label
+              className={styles.label}
+              htmlFor="email"
+            >
+              {labelForEmail ? '' : 'email'}
+            </label>
           </div>
           <div className={styles.form_field}>
-            <input className={styles.input_text} id="company" type="text" name="company" />
-            <label className={styles.label} htmlFor="company">company name</label>
+            <input
+              onChange={(e) => setLabelForCompany(e.target.value)}
+              className={styles.input_text}
+              id="company"
+              type="text"
+              name="company"
+            />
+            <label
+              className={styles.label}
+              htmlFor="company"
+            >
+              {labelForCompany ? '' : 'company name'}
+            </label>
           </div>
           <div className={styles.form_field}>
-            <input className={styles.input_text} id="phone" type="text" name="phone" />
-            <label className={styles.label} htmlFor="phone">phone number</label>
+            <input
+              onChange={(e) => setLabelForPhone(e.target.value)}
+              className={styles.input_text}
+              id="phone"
+              type="text"
+              name="phone"
+            />
+            <label
+              className={styles.label}
+              htmlFor="phone"
+            >
+              {labelForPhone ? '' : 'phone number'}
+            </label>
           </div>
           <div className={styles.form_field}>
-            <input className={styles.input_text} id="message" type="text" name="message" required="required" />
-            <label className={styles.label} htmlFor="message">your message</label>
+            <input
+              onChange={(e) => setLabelForMessage(e.target.value)}
+              className={styles.input_text}
+              id="message"
+              type="text"
+              name="message"
+              required="required"
+            />
+            <label
+              className={styles.label}
+              htmlFor="message"
+            >
+              {labelForMessage ? '' : 'your message'}
+            </label>
           </div>
           <div className={styles.form_field}>
             <input
